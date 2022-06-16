09bcb3e497bf13dc4b16023e0b2723572823d8e3
abrhamgg <abrhamgg@gmail.com>
2022-06-16 11:34:29 +0300

    push and pint

---
+++ monty.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+#define _GNU_SOURCE
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 if success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	stack_t *stack = NULL;
+	char *opcode;
+	char *n;
+	unsigned int line_number;
+	char line[128];
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	line_number = 0;
+	while (fgets(line, sizeof(line), fp))
+	{
+		line_number++;
+		opcode = strtok(line, DELIMITERS);
+		if (opcode == NULL || strncmp(opcode, "#", 1) == 0)
+			continue;
+		while (opcode != NULL)
+		{
+			if (strcmp(opcode, "push") == 0)
+			{
+				n = strtok(NULL, DELIMITERS);
+				push(&stack, line_number, n);
+			}
+			else
+			{
+				opcode_getfunc(opcode, &stack, line_number);
+			}
+			opcode = strtok(NULL, DELIMITERS);
+		}
+	}
+	return (EXIT_SUCCESS);
+}
---
+++ stack_fucn.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+
+/**
+ * push - add new item to top of stack
+ * @stack: pointer to stack
+ * @line_number: line number of instruction
+ * @n: value of new item in stack
+ * Return: void
+ */
+
+void push(stack_t **stack, unsigned int line_number, char *n)
+{
+	stack_t *new = NULL;
+	int i;
+
+	if (n == NULL)
+	{
+		printf("L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[0] == '-' && i == 0)
+			continue;
+		if (isdigit(n[i] == 0))
+		{
+			printf("L%d: usage: push integer\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new->n = atoi(n);
+	new->prev = NULL;
+	new->next = NULL;
+	if (*stack != NULL)
+	{
+		new->next = *stack;
+		(*stack)->prev = new;
+	}
+
+	*stack = new;
+}
